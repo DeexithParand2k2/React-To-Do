420319ce4b7470a57429db6291d249a27f68c043
DeexithParand2k2 <deexith2002@gmail.com>
2022-08-23 15:29:46 +0530

    connected with json server

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React,{ useState } from "react";
+import React,{ useEffect, useState } from "react";
 import AddTask from "./components/AddTask";
 import Header from './components/Header';
 import Tasks from './components/Tasks';
@@ -10,53 +10,72 @@ function App() {
   })
 
   //each task is acts as a seperate state
-  const [taskList,changeTask] = useState(()=>{
-    return [
-      {
-        "id": 1,
-        "text": "Doctors Appointment",
-        "day": "Feb 5th at 2:30pm",
-        "reminder": true
-      },
-      {
-        "id": 2,
-        "text": "Meeting at School",
-        "day": "Feb 6th at 1:30pm",
-        "reminder": true
-      },
-      {
-        "id": 3,
-        "text": "Bhu's appointment",
-        "day": "Jan 3rd at 2:30pm",
-        "reminder": true
-      },
-      {
-        "id": 4,
-        "text": "Sanjay Appointment",
-        "day": "Dec 4th at 1:30pm",
-        "reminder": true
-      },
-    ]
-  })
+  const [taskList,changeTask] = useState([])
+
+  //***fetch tasks from json server***
+  useEffect(()=>{
+
+    //call the getResponse function
+    //which returns a promise
+    //so create a async function => so that i can have promise as a return type
+    const getWorkDone = async () => {
+      const retPromise = await getResponse();
+      //now you can setState
+      changeTask(retPromise)
+    }
+
+    getWorkDone();
+
+  },[])
+
+  //get data function
+  const getResponse = async () =>{
+    const response = await fetch('http://localhost:5000/taskList')
+    const data = await response.json()
+    return data;
+  }
 
   //delete function for tasks
   const deleteTask = (id) =>{
+
+    //modify the json server using method:DELETE
+    //use fetch and get the element with that id
+    //returns promise so use async function
+    const getTaskFromServer = async () =>{
+      await fetch(`http://localhost:5000/taskList/${id}`,{method:'DELETE'})
+    }
+
+    getTaskFromServer();
+
     changeTask(taskList.filter((eachTask)=>eachTask.id !== id));
     console.log(taskList.length)
   }
 
   //add task
-  const addNewTask = (task) =>{
+  const addNewTask = async (task) =>{
     //just now in object format
-    console.log(task);
+    //console.log(task);
 
+    //after passing task has three state variable passed
+    //id is added after coming here const => `newObj = {...task,id}`
+
+    /* TO add task in UI only
     const id = Math.floor(Math.random() * 10000) + 1;
-
     const newOBj = {...task,id} 
+    console.log(newOBj);*/
 
-    console.log(newOBj);
+      
+    const res = await fetch(`http://localhost:5000/taskList`,{
+      method : 'POST',
+      headers : {
+        'content-type':'application/json'
+      },
+      body : JSON.stringify(task)
+    })
+    
+    const data = await res.json()
 
-    changeTask([...taskList,newOBj]);
+    changeTask([...taskList,data]);
   }
 
   return (
--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 
 //why the state variable must be the same
 
